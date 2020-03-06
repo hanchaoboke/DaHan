d0907e4648994a64120972411796d943282d3d53
hanchao <1035710787@qq.com>
2020-03-06 17:33:59 +0800

    两个数交换的三种方法

--- JavaBasics/src/Swap.java
+++ JavaBasics/src/Swap.java
@@ -7,13 +7,43 @@ public class Swap {
     public static void main(String[] args) {
         int x = 5;
         int y = 6;
-        int res = 0xBCD + 0x2FF;
-        System.out.println(res);
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+        System.out.println("交换过后：");
+        swapOne(x,y);
+        swapTwo(x,y);
+        swapThree(x,y);
     }
+
     /**
      * 第一种交换方法
      */
-    void Swap1(int a, int b){
-
+    public static void swapOne(int a, int b){
+        System.out.println("第一种方法");
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("x = "+a+", y = "+b);
+    }
+    /**
+     * 第二种交换方法
+     */
+    public static void swapTwo(int a, int b){
+        System.out.println("第二种方法");
+        a = a ^ b;
+        b = b ^ a;
+        a = a ^ b;
+        System.out.println("x = "+a+", y = "+b);
+    }
+    /**
+     * 第三种交换方法
+     */
+    public static void swapThree(int a, int b){
+        System.out.println("第三种方法");
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("x = "+a+", y = "+b);
     }
 }
