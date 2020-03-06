bba38913528d816c1a9e004b6c853fe6d1beaca4
hanchao <1035710787@qq.com>
2020-03-06 17:08:47 +0800

    交换两个数的三种方法

---
+++ JavaBasics/src/Swap.java
@@ -0,0 +1,19 @@
+/**
+ * @author CXY
+ * @date 2020-03-06 16:34
+ * 描述信息：
+ */
+public class Swap {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        int res = 0xBCD + 0x2FF;
+        System.out.println(res);
+    }
+    /**
+     * 第一种交换方法
+     */
+    void Swap1(int a, int b){
+
+    }
+}
